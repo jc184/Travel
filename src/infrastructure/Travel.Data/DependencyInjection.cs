c93199deb260c09ea756bb0ceb85449804108b78
james chalmers <james.chalmers184@gmail.com>
2022-07-22 06:50:25 +0100

    apply stash

--- src/infrastructure/Travel.Data/DependencyInjection.cs
+++ src/infrastructure/Travel.Data/DependencyInjection.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Travel.Application.Common.Interfaces;
 using Travel.Data.Contexts;
